250b8f9f74c264b8a381b1a03801e72a0f4b4bde
kunalsinha01 <kunalsinha9310@gmail.com>
2025-08-04 18:01:39 +0530

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,19 @@
+/**
+ * Unit Tests for TicTacToeBoard
+**/
+
+#include <gtest/gtest.h>
+#include "TicTacToeBoard.h"
+ 
+class TicTacToeBoardTest : public ::testing::Test
+{
+	protected:
+		TicTacToeBoardTest(){} 
+		virtual ~TicTacToeBoardTest(){} 
+		virtual void SetUp(){} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor) 
+};
+
+TEST(TicTacToeBoardTest, AlwaysFailing){
+	ASSERT_TRUE(false);
+}
